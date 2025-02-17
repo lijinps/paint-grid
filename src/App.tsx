a963204424f04116002eaad764fb6c7f2b88f2a7
lijinps <lijin@ispg.in>
2025-02-17 23:12:16 +0530

    working version

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ export default function App() {
     });
     for (let i = 0; i < noRows; i++) {
       for (let j = 0; j < noCols; j++) {
-        paintAdjecent(i, j);
+        paintAdjecent(i, j, getRandomColor());
       }
     }
     // paintAdjecent(2, 1, getRandomColor());
@@ -59,14 +59,15 @@ export default function App() {
 
   const paintAdjecent = (
     row: number,
-    col: number
-
+    col: number,
+    color: string
     // direction: [number, number]
   ) => {
+    tableGrid[row][col].color = color;
     tableGrid[row][col].isVisited = true;
-    if (row < 0 || col < 0 || row > noRows || col > noCols) {
-      return;
-    }
+    // if (row < 0 || col < 0 || row > noRows || col > noCols) {
+    //   return;
+    // }
 
     for (let i = 0; i < directions.length; i++) {
       const r = row + directions[i][0];
@@ -79,7 +80,7 @@ export default function App() {
         tableGrid[row][col]?.letter === tableGrid[r][c].letter &&
         !tableGrid[r][c].isVisited
       ) {
-        paintAdjecent(r, c);
+        paintAdjecent(r, c, color);
       } else if (
         r < noRows &&
         r > 0 &&
@@ -91,9 +92,9 @@ export default function App() {
         tableGrid[row][col].color = tableGrid[r][c].color;
       }
     }
-    if (tableGrid[row][col].color === "") {
-      tableGrid[row][col].color = getRandomColor();
-    }
+    // if (tableGrid[row][col].color === "") {
+    //   tableGrid[row][col].color = getRandomColor();
+    // }
   };
 
   useEffect(() => {
