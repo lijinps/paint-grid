ad18400f893a90b2170b7b27fa812006d8604e2b
lijinps <lijin@ispg.in>
2025-02-19 00:55:15 +0530

    solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -10,6 +10,7 @@ interface ICell {
 export default function App() {
   const words = ["abbac", "caabc", "abacc", "bbcca", "abcac"];
   const usedColors = new Set();
+  const [distColors, setDistColors] = useState<string[]>([]);
   const [wordCells, setWordCells] = useState<ICell[][]>([]);
   const directions = [
     [0, 1], // right
@@ -17,7 +18,7 @@ export default function App() {
     [1, 0], // bottom
     [-1, 0], // top
   ];
-
+  const isInitialized = useRef(false);
   const getRandomColor = () => {
     let color;
     do {
@@ -33,6 +34,7 @@ export default function App() {
   const tableGrid: ICell[][] = [];
   const noRows = words.length;
   const noCols = words[0].length;
+
   const createCellGrid = () => {
     words.forEach((word, i) => {
       if (!tableGrid[i]) {
@@ -49,74 +51,63 @@ export default function App() {
     });
     for (let i = 0; i < noRows; i++) {
       for (let j = 0; j < noCols; j++) {
-        paintAdjecent(i, j, getRandomColor());
+        if (!tableGrid[i][j].isVisited) {
+          paintAdjacent(i, j, getRandomColor());
+        }
       }
     }
-    // paintAdjecent(2, 1, getRandomColor());
-
+    // paintAdjacent(0, 1, getRandomColor());
+    // console.log("distColors", distColors);
     setWordCells(tableGrid);
   };
 
-  const paintAdjecent = (
-    row: number,
-    col: number,
-    color: string
-    // direction: [number, number]
-  ) => {
-    tableGrid[row][col].color = color;
-    tableGrid[row][col].isVisited = true;
-    // if (row < 0 || col < 0 || row > noRows || col > noCols) {
-    //   return;
-    // }
+  const paintAdjacent = (row: number, col: number, color: string) => {
+    if (!tableGrid[row][col].isVisited) {
+      tableGrid[row][col].color = color;
+      tableGrid[row][col].isVisited = true;
+      setDistColors((prev) => (prev.includes(color) ? prev : [...prev, color]));
+    }
 
-    for (let i = 0; i < directions.length; i++) {
-      const r = row + directions[i][0];
-      const c = col + directions[i][1];
+    for (const [dx, dy] of directions) {
+      const r = row + dx;
+      const c = col + dy;
+
       if (
-        r < noRows &&
-        r > 0 &&
-        c < noCols &&
-        c > 0 &&
-        tableGrid[row][col]?.letter === tableGrid[r][c].letter &&
+        isIndexWithinSize(r, c) &&
+        tableGrid[r][c].letter === tableGrid[row][col].letter &&
         !tableGrid[r][c].isVisited
       ) {
-        paintAdjecent(r, c, color);
-      } else if (
-        r < noRows &&
-        r > 0 &&
-        c < noCols &&
-        c > 0 &&
-        tableGrid[row][col]?.letter === tableGrid[r][c].letter &&
-        tableGrid[r][c].isVisited
-      ) {
-        tableGrid[row][col].color = tableGrid[r][c].color;
+        paintAdjacent(r, c, color);
       }
     }
-    // if (tableGrid[row][col].color === "") {
-    //   tableGrid[row][col].color = getRandomColor();
-    // }
+  };
+  const isIndexWithinSize = (row: number, col: number) => {
+    return row >= 0 && col >= 0 && row < noRows && col < noCols;
   };
 
   useEffect(() => {
-    createCellGrid();
+    if (!isInitialized.current) {
+      createCellGrid();
+      isInitialized.current = true;
+    }
   }, []);
 
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 key={wordCells.length}>No Paints Used: {distColors.length}</h1>
       <div className="grid-container">
-        {wordCells &&
-          wordCells.map((rowItem, i) =>
-            rowItem.map((colItem, j) => (
-              <div
-                key={`${i}-${j}`}
-                className="grid-item"
-                style={{ backgroundColor: colItem.color }}
-              >
-                {colItem?.letter}
-              </div>
-            ))
-          )}
+        {wordCells?.map((rowItem, i) =>
+          rowItem.map((colItem, j) => (
+            <div
+              key={`${i}-${j}`}
+              className="grid-item"
+              style={{ backgroundColor: colItem.color }}
+            >
+              {colItem?.letter}
+              <div>{colItem.color}</div>
+            </div>
+          ))
+        )}
       </div>
     </>
   );
